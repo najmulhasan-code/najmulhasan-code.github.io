61a0c9073f427706787c02b3cb5df0e6f3010ddc
Najmul Hasan <123287444+najmulhasan-code@users.noreply.github.com>
2024-12-25 23:36:29 -0500

    update

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
