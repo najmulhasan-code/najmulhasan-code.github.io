43e9b7234e93b1de9c8ac236b4a69d76dd98c74c
Najmul Hasan <123287444+najmulhasan-code@users.noreply.github.com>
2024-12-25 23:43:32 -0500

    update

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+console.log("Welcome to Najmul Hasan's Portfolio!");
