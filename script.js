a38ad039a75916a46c0e3154d2ab3c33fa40d21f
Najmul Hasan <123287444+najmulhasan-code@users.noreply.github.com>
2025-01-05 00:40:41 -0500

    Updated portfolio

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Console Greeting
 console.log("Welcome to Najmul Hasan's Portfolio!");
+
+// Smooth Scrolling for Links (if added later)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
